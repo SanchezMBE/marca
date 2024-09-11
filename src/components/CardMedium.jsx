4a2399ac1a59fa2b4e46e46501e08c788dcf7075
c3saramirez <cramirez55@ucol.mx>
2024-09-10 23:23:36 -0600

    actualizacion de cards y seccion b

--- src/components/CardMedium.jsx
+++ src/components/CardMedium.jsx
@@ -1,14 +1,12 @@
-// eslint-disable-next-line react/prop-types
 const CardMedium = ({ imageSrc, title, text }) => {
   return (
-    <div className="bg-red-200 shadow-md">
-      <img src={imageSrc} alt="Imagen del CardMedium" className=""/>
-      <div className="p-4">
-        <h5 className="text-gray-900 font-bold">{title}</h5>
-        <p className="text-gray-700">{text}</p>
-      </div>
+    <div className="flex flex-col items-center justify-center bg-red-500 p-6 rounded-lg shadow-md transform transition duration-200 hover:scale-95 cursor-pointer">
+      <img src={imageSrc} alt={title} className="w-full h-48 object-cover mb-4 rounded-md" />
+      <h2 className="text-xl font-bold text-center text-white">{title}</h2>
+      <p className="text-sm text-center text-white">{text}</p>
     </div>
   );
 };
 
 export default CardMedium;
+
--- src/components/CardSmall.jsx
+++ src/components/CardSmall.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line react/prop-types
-const CardSmall = ({ title, author, comments, imageSrc }) => {
+/*const CardSmall = ({ title, author, comments, imageSrc }) => {
   return (
     <div className="bg-white p-4 shadow-md flex items-center">
       <img
@@ -14,6 +14,19 @@ const CardSmall = ({ title, author, comments, imageSrc }) => {
           <span className="text-xs text-gray-500">{comments} comments</span>
         </div>
       </div>
+    </div>
+  );
+};
+
+export default CardSmall;
+*/
+const CardSmall = ({ title, author, comments, imageSrc }) => {
+  return (
+    <div className="flex flex-col items-center justify-center bg-white p-4 rounded-lg shadow-md transform transition duration-200 hover:scale-95 cursor-pointer">
+      <img src={imageSrc} alt={title} className="w-full h-32 object-cover mb-4 rounded-md" />
+      <h2 className="text-lg font-bold text-center">{title}</h2>
+      <p className="text-sm text-gray-600 text-center">{author}</p>
+      <p className="text-sm text-gray-600 text-center">{comments} comentarios</p>
     </div>
   );
 };
